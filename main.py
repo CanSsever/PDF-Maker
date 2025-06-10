f042a163bb7aaacde7ce1ef6c2fdfd946a6792b0
Can Sever <cansevver01@gmail.com>
2025-06-10 13:12:10 +0200

    add footers for each page

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv("topics.csv")
 
@@ -14,9 +15,19 @@ for index, row in df.iterrows():
     pdf.cell(w=0, h=12, txt=row['Topic'], align="L", ln=1)
     pdf.line(10, 21, 200, 21) ## X1, Y1 ve X2, Y2
 
+    # set the footer
+    pdf.ln(265)
+    pdf.set_font(family="Times", style="I", size=10)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=10, txt=row['Topic'], align="R")
 
     for i in range(row['Pages'] - 1):
         pdf.add_page()
+
+        pdf.ln(277)
+        pdf.set_font(family="Times", style="I", size=10)
+        pdf.set_text_color(100, 100, 100)
+        pdf.cell(w=0, h=10, txt=row['Topic'], align="R")
 
 pdf.output("output.pdf")
 
