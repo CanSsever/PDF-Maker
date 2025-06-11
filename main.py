27fc1df71322b92f7968e88481b2d145db6e3986
Can Sever <cansevver01@gmail.com>
2025-06-11 13:58:41 +0200

    add page number for each page

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv("topics.csv")
+page_number = 0
 
 ## set header
 for index, row in df.iterrows():
@@ -18,11 +19,13 @@ for index, row in df.iterrows():
     for y in range(20, 292, 8):
         pdf.line(10, y, 200, y)
 
+
     # set the footer
     pdf.ln(265)
     pdf.set_font(family="Times", style="I", size=10)
     pdf.set_text_color(100, 100, 100)
-    pdf.cell(w=0, h=10, txt=row['Topic'], align="R")
+    pdf.cell(w=0, h=10, txt=f"{row['Topic']} - Page {page_number}", align="R")
+    page_number += 1
 
     for i in range(row['Pages'] - 1):
         pdf.add_page()
@@ -30,7 +33,8 @@ for index, row in df.iterrows():
         pdf.ln(277)
         pdf.set_font(family="Times", style="I", size=10)
         pdf.set_text_color(100, 100, 100)
-        pdf.cell(w=0, h=10, txt=row['Topic'], align="R")
+        pdf.cell(w=0, h=10, txt=f"{row['Topic']} - Page {page_number}", align="R")
+        page_number += 1
 
         for y in range(20, 292, 8):
             pdf.line(10, y, 200, y)
